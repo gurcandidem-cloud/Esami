efb077065b91310eb15d120d13c69f3f1cbf30b6
gurcandidem-cloud <gurcandidem@gmail.com>
2025-12-28 17:51:40 +0300

    Add files via upload

---
+++ ogrenciler.js
@@ -0,0 +1,16 @@
+// --- ÖĞRENCİ LİSTESİ DOSYASI ---
+// Buraya sadece öğrencilerin isimlerini yazınız.
+// Gizli şifreler (R***_**** vb.) buraya YAZILMAZ.
+
+const OGRENCI_LISTESI = [
+    "AZİME ERTUGAY",
+    "ELIF KÖSEO",
+    "ELIF TASKIN KARACAY",
+    "GIZEM AYDIN",
+    "GULBIN OZTURK",
+    "IREM CINER",
+    "LEYLA ŞENGÜL COŞKUN",
+    "ÖZGÜR BARIS AYDIN",
+    "ÖZLEM ILBI",
+    "ŞÜKRÜ ÖZKAN"
+];
